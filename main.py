5caa8b459256ba06bb6f48c449f7a49f1546bf2a
omr746 <112737842+omr746@users.noreply.github.com>
2026-02-14 09:14:41 +0200

    update

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes import base
+
 app=FastAPI()
-@app.get("/welcome")
-def welcome():
-    return{
-        "message":"hello"
-    }
+
+app.include_router(base.base_router)
+
  
